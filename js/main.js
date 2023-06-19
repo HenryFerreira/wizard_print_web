631ffe4f45802ff2d24e138347a23abd912ca5a5
HaffCode <haff.code.uy@gmail.com>
2023-06-19 20:54:22 -0300

    Se termina el HTML y CSS

---
+++ js/main.js
@@ -0,0 +1,66 @@
+// PRODUCTOS
+const productos = [
+    // Busos
+    {
+        id: "buzo-01",
+        titulo: "Buzo 01",
+        imagen: "./img/buzos/01.jpg",
+        categoria: {
+            nombre: "BUZO",
+            id: "buzos"
+        },
+        precio: 1000
+    },
+    {
+        id: "buzo-02",
+        titulo: "Buzo 02",
+        imagen: "./img/buzos/02.jpg",
+        categoria: {
+            nombre: "BUZO",
+            id: "buzos"
+        },
+        precio: 1000
+    },
+    // Pines
+    {
+        id: "pin-01",
+        titulo: "Pin 01",
+        imagen: "./img/pines/01.jpg",
+        categoria: {
+            nombre: "PIN",
+            id: "pines"
+        },
+        precio: 1000
+    },
+    {
+        id: "pin-02",
+        titulo: "Pin 02",
+        imagen: "./img/pines/02.jpg",
+        categoria: {
+            nombre: "PIN",
+            id: "pines"
+        },
+        precio: 1000
+    },
+    // Tazas
+    {
+        id: "taza-01",
+        titulo: "Taza 01",
+        imagen: "./img/tazas/01.jpg",
+        categoria: {
+            nombre: "TAZA",
+            id: "tazas"
+        },
+        precio: 1000
+    },
+    {
+        id: "taza-02",
+        titulo: "Taza 02",
+        imagen: "./img/tazas/02.jpg",
+        categoria: {
+            nombre: "TAZA",
+            id: "tazas"
+        },
+        precio: 1000
+    }
+];
