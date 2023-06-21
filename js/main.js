e40bbcb5e752d91e684bf4b81d0f2c041934fc12
HaffCode <haff.code.uy@gmail.com>
2023-06-20 21:29:43 -0300

    Se implementa la API para obtener los productos y se avanza con el JS

--- js/main.js
+++ js/main.js
@@ -1,66 +1,85 @@
-// PRODUCTOS
-const productos = [
-    // Busos
-    {
-        id: "buzo-01",
-        titulo: "Buzo 01",
-        imagen: "./img/buzos/01.jpg",
-        categoria: {
-            nombre: "BUZO",
-            id: "buzos"
-        },
-        precio: 1000
-    },
-    {
-        id: "buzo-02",
-        titulo: "Buzo 02",
-        imagen: "./img/buzos/02.jpg",
-        categoria: {
-            nombre: "BUZO",
-            id: "buzos"
-        },
-        precio: 1000
-    },
-    // Pines
-    {
-        id: "pin-01",
-        titulo: "Pin 01",
-        imagen: "./img/pines/01.jpg",
-        categoria: {
-            nombre: "PIN",
-            id: "pines"
-        },
-        precio: 1000
-    },
-    {
-        id: "pin-02",
-        titulo: "Pin 02",
-        imagen: "./img/pines/02.jpg",
-        categoria: {
-            nombre: "PIN",
-            id: "pines"
-        },
-        precio: 1000
-    },
-    // Tazas
-    {
-        id: "taza-01",
-        titulo: "Taza 01",
-        imagen: "./img/tazas/01.jpg",
-        categoria: {
-            nombre: "TAZA",
-            id: "tazas"
-        },
-        precio: 1000
-    },
-    {
-        id: "taza-02",
-        titulo: "Taza 02",
-        imagen: "./img/tazas/02.jpg",
-        categoria: {
-            nombre: "TAZA",
-            id: "tazas"
-        },
-        precio: 1000
-    }
-];
+/* Elementos del HTML */
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+
+/* URLs de la API */
+const URL_API_GETALL_PRODUCTOS = "https://apinode-production-03a5.up.railway.app/api/productos"
+
+/* Funciones */
+async function cargarProductos() {
+    const respuesta = await fetch (URL_API_GETALL_PRODUCTOS);
+    const data = await respuesta.json();
+
+    contenedorProductos.innerHTML = "";
+
+    data.forEach(elem =>{ 
+        console.log(elem.titulo)
+        
+        const div =  document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = 
+        `
+            <img class="producto-imagen" src="./images/buzos/01.png" alt="">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${elem.titulo}</h3>
+                <p class="producto-precio">$ ${elem.precio}</p>
+                <button class="producto-agregar" id="${elem._id}">
+                    <i class="bi bi-plus-square-fill"></i>
+                    Agregar
+                </button>
+            </div>
+        `;
+        contenedorProductos.append(div);
+    })
+}
+
+async function cargarProductosPorCategoria(categoria) {
+    const respuesta = await fetch (URL_API_GETALL_PRODUCTOS);
+    const data = await respuesta.json();
+    const dataFilter = await data.filter(elem => elem.categoria === categoria);
+
+    contenedorProductos.innerHTML = "";
+
+    dataFilter.forEach(elem =>{ 
+        const div =  document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = 
+        `
+            <img class="producto-imagen" src="./images/buzos/01.png" alt="">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${elem.titulo}</h3>
+                <p class="producto-precio">$ ${elem.precio}</p>
+                <button class="producto-agregar" id="${elem._id}">
+                    <i class="bi bi-plus-square-fill"></i>
+                    Agregar
+                </button>
+            </div>
+        `;
+        tituloPrincipal.innerText = categoria;
+        contenedorProductos.append(div);
+    })
+}
+
+
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (evento) => {
+        botonesCategorias.forEach(boton=> {
+            boton.classList.remove("active")
+        });
+        evento.currentTarget.classList.add("active");
+
+        if(evento.currentTarget.id === "todos-productos"){
+            tituloPrincipal.innerText = "Todos los productos"
+            cargarProductos()
+        }
+        else{
+            cargarProductosPorCategoria(evento.currentTarget.id);
+        }
+
+    });
+});
+  
+
+cargarProductos()
